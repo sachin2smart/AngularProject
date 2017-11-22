8ba3ad148615fd18afd65b6ccd0b3ac11042df2e
Paikray <guruprasad.paikray@capgemini.com>
2017-11-22 17:25:29 +0530

    first angular checkin

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector : 'pm-root',
+    template :`
+        <div> 
+            <h1>{{pageTitle}}</h1>
+            <div>My first component </div>
+        </div>
+    `
+})
+
+export class AppComponent {
+    pageTitle : String = 'ACME Product Management';
+}
